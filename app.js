21e4ce26091aa1cc783430c44691b6c79229deba
Harish Singh <singhharish734@gmail.com>
2022-01-27 21:53:29 +0530

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-function showIt(){
-  var result= document.getElementById("textarea").value;
-  alert(result);
+function genQR() {
+  var url = "https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs="
+
+  var image= document.getElementById("img")
+  var textIn= document.getElementById("qrtext").value;
+  var imgSize= document.getElementById("size").value;
+
+  if(textIn!="" && imgSize=="100"){
+    image.src=url+"100x100"+"&ch1="+textIn;
+    // https://chart.googleapis.com/chart?cht=qr&chs=100x100&ch1=hello
+  }
+  else if(textIn!="" && imgSize=="150"){
+    image.src=url+"150x150"+"&ch1="+textIn;
+  }
+  else if(textIn!="" && imgSize=="256"){
+    image.src=url+"256x256"+"&ch1="+textIn;
+  }
+  else if(textIn!="" && imgSize=="512"){
+    image.src=url+"512x512"+"&ch1="+textIn;
+  }
+  else {
+    alert("Please Enter Text !");
+  }
 }
